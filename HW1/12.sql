aaee165bfaa27f5ab731fa489b29917d4726ebc6
Celine <86948988+Celine10811020@users.noreply.github.com>
2023-03-28 17:37:10 +0800

    Update 12.sql

--- HW1/12.sql
+++ HW1/12.sql
@@ -207,3 +207,121 @@ WHERE Pitcher_Id NOT IN (
 )
 ORDER BY avg_st_pc DESC
 LIMIT 20;
+
+/* 會幾種投球方式 */
+SELECT
+  Id AS Pitcher_id,
+  Pitcher_name,
+  num_type
+FROM (
+  /* 2021年中，每一位投手會幾種投球方式 */
+  SELECT
+    Pitcher AS Pitcher_name,
+    num_type
+  FROM (
+    /* 計算選手會幾種投球方式 */
+    SELECT
+      Pitcher,
+      COUNT(_Type) AS num_type
+    FROM (
+      /* 2021年上場的投手投過的球種 */
+      SELECT DISTINCT
+        Pitcher,
+        _Type
+      FROM pitches
+      JOIN (
+        SELECT *
+        FROM games
+        WHERE DATE_FORMAT(games.Date, '%Y') = "2021"
+      ) AS 2021_game
+      ON pitches.Game = 2021_game.Game
+    ) AS pitcher_with_pitch_result
+    GROUP BY Pitcher
+    ORDER BY Pitcher
+  ) AS pitcher_num_type
+) AS pitch_type
+JOIN (
+  /* ID與名字的對應 */
+  SELECT 
+    Id, 
+    SUBSTRING(Name, LOCATE('.', Name) + 2, LENGTH(Name)) AS name 
+  FROM players 
+) AS player_name
+ON player_name.name = pitch_type.Pitcher_name
+WHERE Id NOT IN (
+   /* 找到先發次數大於10的選手 */
+   SELECT
+     Pitcher_id
+   FROM (
+      /* 計算2021年中投手先發的次數 */
+      SELECT
+        pitcher_name,
+        Pitcher_id,
+        COUNT(Pitcher_id) AS xian_fa_ci_shu
+      FROM (
+         /* 2021年中有當過先發投手的名字與ID */
+         SELECT
+           pitcher_name_2021_game.game_id AS game_id,
+           pitcher_name_2021_game.pitcher_name AS pitcher_name,
+           Pitcher_id
+         FROM (
+            /* 找出在2021年中當過start的選手名字 */
+            SELECT DISTINCT
+               Pitcher AS pitcher_name,
+               pitches.Game AS game_id,
+               Inning
+            FROM pitches
+            JOIN (
+              /* 2021年中有上場的選手的ID與名字 */
+              SELECT
+                Game
+              FROM
+                games
+              WHERE
+                DATE_FORMAT(games.Date, '%Y') = "2021"
+            ) AS game_2021
+            ON pitches.Game = game_2021.Game
+            WHERE 
+              Inning = "T1" OR
+              Inning = "B1"
+         ) AS pitcher_name_2021_game
+         JOIN (
+            SELECT
+              game_id,
+              Pitcher_id,
+              player_name
+            FROM (
+              SELECT
+                pitchers.Game AS game_id,
+                Pitcher_id
+              FROM
+                pitchers
+              JOIN (
+                SELECT
+                  Game
+                FROM
+                  games
+                WHERE
+                  DATE_FORMAT(games.Date, '%Y') = "2021"
+              ) AS game_2021
+              ON pitchers.Game = game_2021.Game
+            ) AS game_2021_pitcher_id
+            JOIN (
+              SELECT 
+                Id, 
+                SUBSTRING(Name, LOCATE(' ', Name) + 1, LENGTH(Name)) AS player_name 
+              FROM players
+            ) AS pitcher_id_player_name
+            ON game_2021_pitcher_id.Pitcher_id = pitcher_id_player_name.Id
+         ) AS game_id_Pitcher_id_player_name
+         ON 
+           pitcher_name_2021_game.game_id = game_id_Pitcher_id_player_name.game_id AND
+           pitcher_name_2021_game.pitcher_name = game_id_Pitcher_id_player_name.player_name
+      ) AS xian_fa_pitcher
+      GROUP BY 
+        pitcher_name,
+        Pitcher_id
+   ) AS pitcher_with_xian_fa_ci_shu
+   WHERE xian_fa_ci_shu > 10
+) AND
+ID = "35222" OR ID = "41284"
