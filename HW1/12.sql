e6b95d4ebafec49ee40a7db4ee4edcc65b41dde0
Celine <86948988+Celine10811020@users.noreply.github.com>
2023-03-28 17:10:06 +0800

    Update 12.sql

--- HW1/12.sql
+++ HW1/12.sql
@@ -99,33 +99,61 @@ WHERE Pitcher_Id NOT IN (
 )
 ORDER BY avg_K_IP DESC
 LIMIT 20;
-  
---前20高的avg_st_pc
+
+/* avg_st_pc前二十的選手 */
 SELECT
-  player_list.Pitcher_id,
-  pitcher_name,
+  PC_ST.Pitcher_id AS Pitcher_id,
+  Name AS Pitcher_name,
   ROUND(avg_st_pc, 4) AS `avg_ST/PC`
 FROM (
+  /* 2021年中，每一位投手的AVG(ST/PC) */
+  SELECT
+    Pitcher_Id,
+    AVG(ST / PC) AS avg_st_pc
+  FROM (
+    SELECT
+      Pitcher_Id,
+      CAST(SUBSTRING_INDEX(PC_ST, '-', 1) AS UNSIGNED) AS PC,
+      CAST(SUBSTRING_INDEX(PC_ST, '-', -1) AS UNSIGNED) AS ST
+    FROM
+      pitchers
+    JOIN (
+       SELECT *
+       FROM games
+       WHERE DATE_FORMAT(games.Date, '%Y') = "2021"
+    ) AS 2021_game
+    ON pitchers.Game = 2021_game.Game
+  ) AS pitcher_with_pc_st
+  GROUP BY Pitcher_Id
+  ORDER BY avg_st_pc DESC
+) AS PC_ST
+JOIN players
+ON players.Id = PC_ST.Pitcher_Id
+WHERE Pitcher_Id NOT IN (
+   /* 找到先發次數大於10的選手 */
    SELECT
-     Pitcher_id,
-     pitcher_name
+     Pitcher_id
    FROM (
+      /* 計算2021年中投手先發的次數 */
       SELECT
         pitcher_name,
         Pitcher_id,
         COUNT(Pitcher_id) AS xian_fa_ci_shu
       FROM (
+         /* 2021年中有當過先發投手的名字與ID */
          SELECT
            pitcher_name_2021_game.game_id AS game_id,
            pitcher_name_2021_game.pitcher_name AS pitcher_name,
            Pitcher_id
          FROM (
+            /* 找出在2021年中當過start的選手名字 */
             SELECT DISTINCT
                Pitcher AS pitcher_name,
                pitches.Game AS game_id,
                Inning
             FROM pitches
             JOIN (
+              /* 2021年中有上場的選手的ID與名字 */
               SELECT
                 Game
               FROM
@@ -175,29 +203,7 @@ FROM (
         pitcher_name,
         Pitcher_id
    ) AS pitcher_with_xian_fa_ci_shu
-   WHERE xian_fa_ci_shu <= 10
- ) player_list
- JOIN (
-    SELECT
-      Pitcher_Id,
-      AVG(ST / PC) AS avg_st_pc
-    FROM (
-      SELECT
-        Pitcher_Id,
-        CAST(SUBSTRING_INDEX(PC_ST, '-', 1) AS UNSIGNED) AS PC,
-        CAST(SUBSTRING_INDEX(PC_ST, '-', -1) AS UNSIGNED) AS ST
-      FROM
-        pitchers
-      JOIN (
-         SELECT *
-         FROM games
-         WHERE DATE_FORMAT(games.Date, '%Y') = "2021"
-      ) AS 2021_game
-      ON pitchers.Game = 2021_game.Game
-   ) AS pitcher_with_pc_st
-   GROUP BY Pitcher_Id
-   ORDER BY avg_st_pc DESC
- ) AS PC_ST
- ON player_list.Pitcher_id = PC_ST.Pitcher_Id
- ORDER BY avg_st_pc DESC
- LIMIT 20;
+   WHERE xian_fa_ci_shu > 10
+)
+ORDER BY avg_st_pc DESC
+LIMIT 20;
